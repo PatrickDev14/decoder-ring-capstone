bb8900871c201af007eb4dd2f198394530910d94
Patrick Lauher <plauher14@gmail.com>
2021-12-12 20:29:53 -0600

    complete caesar for'encode=true'; with .split, .map, .join

--- src/caesar.js
+++ src/caesar.js
@@ -4,14 +4,21 @@
 // of the anonymous function on line 6
 
 /* shift one input string into another string for encode, shift the opposite way for decode
+function returns false if shift isn't present, or === 0, less than -25, or > 25
 turn strings into arrays, to use methods?
 split method for input string => array?
-check if character is alphanumeric, then it will be shifted, do something with ascii
+use .map(the bulk- check if character is alphanumeric, then it will be shifted, do something with ascii) then .join at the end
+
 use to.lowercase to avoid bigger ascii numbers of capitalized characters?
+decipher case for space === 32, use no shift
+decipher case for small letters, >= 97 && <= 122, use shift
+decipher case for other symbols, >= 33 && <= 96, use no shift
+
+
 shift from end of alphabet should wrap to front of alphabet
 join method for array => string output
 to use decode operation, the encode parameter === false
-function returns false if shift isn't present, or === 0, less than -25, or > 25
+
 */
 
 
@@ -21,7 +28,31 @@ const caesarModule = (function () {
   function caesar(input, shift, encode = true) {
     // your solution code here
     if (!shift || shift === 0 || shift > 25 || shift < -25) return false;
+
+    if (encode === false) {
+      shift = shift * (-1);
+    }
+
+    let lowerMsg = input.toLowerCase();
+
+    return lowerMsg.split("")
+      .map((char) => {
+        let asciiNum = char.charCodeAt();
+        if (asciiNum >= 32 && asciiNum <= 96) {
+          return String.fromCharCode(asciiNum);
+
+        } else if (asciiNum + shift < 97) {
+            return String.fromCharCode(asciiNum + shift + 26);
+        } else if (asciiNum + shift > 122) {
+            return String.fromCharCode(asciiNum + shift - 26);
+        } else if (asciiNum + shift >= 97 && asciiNum + shift <= 122) {
+            return String.fromCharCode(asciiNum + shift);
+        }
+      }
+
+      ).join("");
   }
+
 
   return {
     caesar,
