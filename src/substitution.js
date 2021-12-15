6b3ddf3ab703044289672e25f4149183b50cd299
Patrick Lauher <plauher14@gmail.com>
2021-12-15 10:32:15 -0600

    update substitution for alphabet tests, presence, length, unique

--- src/substitution.js
+++ src/substitution.js
@@ -3,22 +3,22 @@
 // Only add code (helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 /* input can contain letters, spaces, special char; alphabet paramater can contain letters, spaces, special char; 
-alphabet.length must === 26 or return false; all of alphabet char must be unique;
-how to check unique in string? 
-for (let char in alphabet) { 
-    const character = alphabet[char] 
-    if (alphabet.slice(char + 1).includes(character)) { 
-      return false;
-  or
-let checkforDupes = new Set(alphabet)
-    if ([...checkforDupes].length !== 26) return false;
+alphabet.length must === 26 or return false; all of alphabet char must be unique; spaces should be maintained;
+
 */
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    // alphabet tests for presence, length
+    if (!alphabet || alphabet.length !== 26) return false;
+    //alphabet test for containing unique characters
+    for (let char in alphabet) {
+      const character = alphabet[char];
+      if (alphabet.slice(char + 1).includes(character)) return false;
+    }
+    const realAlphabet = "abcdefghijklmnopqrstuvwxyz";
   }
 
   return {
