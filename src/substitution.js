cd0676c9a454ac36527cfe85a6d8df2c0cf50de0
Patrick Lauher <plauher14@gmail.com>
2021-12-15 22:08:07 -0600

    substitution.js encode complete to line 33

--- src/substitution.js
+++ src/substitution.js
@@ -5,12 +5,18 @@
 /* input can contain letters, spaces, special char; alphabet paramater can contain letters, spaces, special char; 
 alphabet.length must === 26 or return false; all of alphabet char must be unique; spaces should be maintained;
 
+map(char) from the input.split() array; realAlphabet.find(letter where char === letter ) = matchedLetter;
+matchedIndex = get indexOf matchedLetter; return alphabet[matchedIndex];
+
 */
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  const realAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
   function substitution(input, alphabet, encode = true) {
+    let matchedLetter;
+    let matchedIndex;
     // alphabet tests for presence, length
     if (!alphabet || alphabet.length !== 26) return false;
     //alphabet test for containing unique characters
@@ -18,7 +24,16 @@ const substitutionModule = (function () {
       const character = alphabet[char];
       if (alphabet.slice(char + 1).includes(character)) return false;
     }
-    const realAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    return input.toLowerCase()
+      .split("")
+      .map((char) => {   //add criterion for space here
+        if (encode) {
+          matchedLetter = realAlphabet.find((letter) => letter === char);
+          matchedIndex = realAlphabet.indexOf(matchedLetter);
+          return alphabet[matchedIndex];
+
+        }
+      })
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,11 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const substitution = require("../src/substitution");
+
+describe("substitution", () => {
+    it("should return false if the substitution alphabet is missing", () => {
+      const message = "firehose day 55"
+      const actual = substitution.substitution(message)
+      expect(actual).to.false;
+    });
+})
