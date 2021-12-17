d0c0c6187669fa41f789c89d9b15136594c2bd77
Patrick Lauher <plauher14@gmail.com>
2021-12-17 01:26:00 -0600

    caesar.test.js finished error handling

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,28 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const caesar = require("../src/caesar");
+
+describe("caesar() submission tests written by PatrickDev14", () => {
+  describe("error handling", () => {
+    it("should return false if the shift amount is 0", () => {
+      const message = "Firehose day 56";
+      const shift = 0;
+      const actual = caesar.caesar(message, shift);
+      expect(actual).to.be.false;    
+  });
+    
+    it("should return false if the shift amount is above 25", () => {
+      const message = "firehose Day 56";
+      const shift = 33;
+      const actual = caesar.caesar(message, shift);
+      expect(actual).to.be.false;  
+    });
+
+    it("should return false if the shift amount is less than -25", () => {
+        const message = "firehose Day 56";
+        const shift = -30;
+        const actual = caesar.caesar(message, shift);
+        expect(actual).to.be.false;  
+      });
+});
+})
