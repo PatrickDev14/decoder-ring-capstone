ca4bde0b6cc8db5a03de950297df5db196eee612
Patrick Lauher <plauher14@gmail.com>
2021-12-17 00:58:02 -0600

    update substitution.test.js tests - error handling

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -2,10 +2,24 @@
 const expect = require("chai").expect;
 const substitution = require("../src/substitution");
 
-describe("substitution", () => {
+describe("substitution() submission tests - error handling", () => {
     it("should return false if the substitution alphabet is missing", () => {
-      const message = "firehose day 55"
-      const actual = substitution.substitution(message)
-      expect(actual).to.false;
+      const message = "firehose day 55";
+      const actual = substitution.substitution(message);
+      expect(actual).to.be.false;
     });
-})
+
+    it("should return false if the substitution alphabet is not exaclty 26 characters", () => {
+        const message = "firehose day 56";
+        const alphabet = "notequalvwxyz";
+        const actual = substitution.substitution(message, alphabet);
+        expect(actual).to.be.false;
+    });
+
+    it("should return false if the substitution alphabet does not contain unique characters", () => {
+        const message = "firehose day 56";
+        const alphabet = "improperlyrepeats";
+        const actual = substitution.substitution(message, alphabet);
+        expect(actual).to.be.false;
+    });
+});
