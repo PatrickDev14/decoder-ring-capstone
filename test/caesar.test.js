549d11000b7d251686124064a9f65a65f652d6c6
Patrick Lauher <plauher14@gmail.com>
2021-12-19 23:10:36 -0600

    finish caesar tests

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -23,6 +23,30 @@ describe("caesar() submission tests written by PatrickDev14", () => {
         const shift = -30;
         const actual = caesar.caesar(message, shift);
         expect(actual).to.be.false;  
-      });
+    });
+  });
+
+  it("should encode a message by shifting the letters", () => {
+    const expected = "massive";
+    const actual = caesar.caesar("Ocuukxg", -2, encode = true);
+    expect(actual).to.eql(expected);
+  });
+
+  it("ignores capital letters", () => {
+    const expected = "massive";
+    const actual = caesar.caesar("Ocuukxg", -2, encode = true);
+    expect(actual).to.eql(expected);
+  });
+
+  it("should appropriately handle letters at the end of the alphabet", () => {
+    const expected = "wamddpi";
+    const actual = caesar.caesar("swizzle", 4, encode = true);
+    expect(actual).to.eql(expected);
+  });
+
+  it("should leave spaces and other symbols as is", () => {
+    const expected = ":a &bc";
+    const actual = caesar.caesar(":c &de", -2, encode = true);
+    expect(actual).to.eql(expected);
+  });
 });
-})
